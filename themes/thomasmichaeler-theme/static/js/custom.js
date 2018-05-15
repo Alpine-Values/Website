876e411e6ba60e8eb04fb986ae6f41767eb2443b
Thomas Michaeler <social@thomasmichaeler.com>
2018-05-14 23:03:39 -0400

    add fluidbox, masonry images, home page

--- themes/thomasmichaeler-theme/static/js/custom.js
+++ themes/thomasmichaeler-theme/static/js/custom.js
@@ -2,9 +2,10 @@ masonry();
 $(function () {
     highlightCurrentPage();
     makeImagesResponsive();
+    alignImageGrid();
 });
 function highlightCurrentPage() {
-  $("a[href='" + location.href + "']").parent().addClass("active");
+  $("a[href='" + location.href + "']").addClass("active");
 }
 function makeImagesResponsive() {
     $("img").addClass("img-responsive");
@@ -17,4 +18,50 @@ function masonry() {
     $grid.imagesLoaded().progress(function () {
         $grid.masonry('layout');
     });
+}
+
+
+function alignImageGrid() {
+  if( $('#article, #about').length ) {
+
+    $(window).on('load', function() {
+
+      $('.content-column-content p').each( function(i){
+        var cont = $(this);
+
+        if( cont.has('img').length ) {
+          cont.addClass('post-masonry');
+          var num = cont.find('img').length;
+          var ratios = new Array('1');
+          var sum = 0;
+
+          cont.find('img').each( function(j){
+            var img = $(this);
+            var src = img.attr('src');
+
+            ratios[j] = (img[0].naturalWidth+10) / (img[0].naturalHeight+10);   // width plus padding
+            sum += ratios[j];
+
+            img.wrap('<a href="'+src+'" />').parent().fluidbox({
+                stackIndex: 100,
+                viewportFill: 0.9
+            });
+
+            if(j >= num-1) {
+              $.each(ratios, function(k, val) {
+                cont.find('a:eq('+k+')').width( (ratios[k] / sum * 100)+'%' );
+              });
+            }
+          });
+
+        } else {
+
+
+        }
+
+      });
+
+    });
+
+  }  
 }
